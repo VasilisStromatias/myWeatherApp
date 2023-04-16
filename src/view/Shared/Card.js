0475f382a1b18109def143e2032b0d55fe6f16ba
VasilisStromatias <stromatiasvasilis@gmail.com>
2023-04-16 19:21:41 +0300

    fetch data from users coords

--- src/view/Shared/Card.js
+++ src/view/Shared/Card.js
@@ -1,12 +1,41 @@
+import React, {useEffect, useState} from "react";
+
+
+
 function Card(){
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+const [data, setData] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1ee16b2b7b1692c2baaa3d0534f0bb02`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
 return(
-    <div className="card-wrapper">
-        <div className="card-inner">
+    <div className="weather-wrapper">
+        {(typeof data.main != 'undefined') ? (
             <p>
-                This is a card 
+                {data.name}
+                {data.main.temp}
             </p>
-        </div>
+      ): (
+        <div>not</div>
+      )}
     </div>
+
 );
 }
 
