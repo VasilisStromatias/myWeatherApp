f04f0d463b62377d45a7b8465c5272c5968b7b83
VasilisStromatias <stromatiasvasilis@gmail.com>
2023-04-17 17:00:27 +0300

    Add date and more fields from the API

--- src/view/Shared/Card.js
+++ src/view/Shared/Card.js
@@ -1,17 +1,19 @@
 import React, {useEffect, useState} from "react";
 
-
-
 function Card(){
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
 const [data, setData] = useState([]);
+
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}`;
 
 useEffect(() => {
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
+        // console.log(lat , long);
       });
 
       await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1ee16b2b7b1692c2baaa3d0534f0bb02`)
@@ -26,13 +28,33 @@ useEffect(() => {
 
 return(
     <div className="weather-wrapper">
-        {(typeof data.main != 'undefined') ? (
-            <p>
-                {data.name}
-                {data.main.temp}
-            </p>
+        {(typeof data.main != 'undefined') ? (         
+            <>
+            <div className="wa-city">
+              <span><strong>{data.name} </strong></span><span>, {data.sys.country}</span>
+            </div>
+
+            <div className="wa-date">
+              {date}
+            </div>
+
+            <div className="wa-temperature">
+            <label>Temp: </label>
+            <span>{data.main.temp} &deg;C</span>
+            </div>
+
+            <div className="wa-humidity">
+              <label>Humid: </label>
+              <span>{data.main.humidity}%</span>
+            </div>
+
+            <div className="wa-conditions">
+             <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt='icon'/>           
+            <span>{data.weather[0].main}</span>
+            </div>
+            </>
       ): (
-        <div>not</div>
+        <div>Not fetched correctly</div>
       )}
     </div>
 
