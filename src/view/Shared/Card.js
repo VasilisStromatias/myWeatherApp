4c5ba1308e7a00856c9e0eb9ef2d5261295fede2
VasilisStromatias <stromatiasvasilis@gmail.com>
2023-04-17 22:27:14 +0300

    Add font family and some styles

--- src/view/Shared/Card.js
+++ src/view/Shared/Card.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react";
 
+
 function Card(){
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
   const current = new Date();
   const date = `${current.getDate()}/${current.getMonth()+1}`;
@@ -13,7 +14,6 @@ useEffect(() => {
       navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
-        // console.log(lat , long);
       });
 
       await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1ee16b2b7b1692c2baaa3d0534f0bb02`)
@@ -27,17 +27,24 @@ useEffect(() => {
   }, [lat,long])
 
 return(
-    <div className="weather-wrapper">
+    <div className="wa-wrapper">
         {(typeof data.main != 'undefined') ? (         
-            <>
+            <div className="wa-inner">
             <div className="wa-city">
-              <span><strong>{data.name} </strong></span><span>, {data.sys.country}</span>
-            </div>
 
+              <span><strong>{data.name} </strong></span><span>, {data.sys.country}</span>
             <div className="wa-date">
               {date}
             </div>
+            
+            </div>
 
+            <div className="wa-conditions">
+             <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt='icon'/>           
+            <span>{data.weather[0].main}</span>
+            </div>
+
+          <div className="wa-temp-hum"> 
             <div className="wa-temperature">
             <label>Temp: </label>
             <span>{data.main.temp} &deg;C</span>
@@ -47,12 +54,10 @@ return(
               <label>Humid: </label>
               <span>{data.main.humidity}%</span>
             </div>
+          </div>
+            
 
-            <div className="wa-conditions">
-             <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt='icon'/>           
-            <span>{data.weather[0].main}</span>
             </div>
-            </>
       ): (
         <div>Not fetched correctly</div>
       )}
