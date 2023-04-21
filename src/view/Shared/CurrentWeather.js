ba0a5a874494c38ff03f689e2d605a59251ae642
VasilisStromatias <stromatiasvasilis@gmail.com>
2023-04-21 16:19:28 +0300

    Display with map the weather forecast

--- src/view/Shared/CurrentWeather.js
+++ src/view/Shared/CurrentWeather.js
@@ -18,7 +18,6 @@ useEffect(() => {
 
       // CURRENT WEATHER
       await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1ee16b2b7b1692c2baaa3d0534f0bb02`) //current position
-      // await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=38.0398760&lon=23.7392700&units=metric&appid=1ee16b2b7b1692c2baaa3d0534f0bb02`) //nea philadelphia
       .then(res => res.json())
       .then(result => {
         setData(result)
--- src/view/Shared/WeatherForecast.js
+++ src/view/Shared/WeatherForecast.js
@@ -1,10 +1,69 @@
 import React,{useState, useEffect} from "react";
 
 function WeatherForecast () {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+
+      const fetchData = async () =>{
+          navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+          });
     
+            await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=8f81e9f0ce84e9bd3fe8f09a74cdd101`) //current position
+          .then(res => res.json())
+          .then(result => {
+            setData(result)
+            console.log(result);
+          });
+        }
+        fetchData();
+  }, [lat,long])
+
+
+
     return (  
         <>
-        hey
+        {(typeof data.city != 'undefined') ? (
+            <div className="wf-wrapper">
+                {data.list.map((item, key) => {
+                    return (
+                        <div className="wf-inner" id={key}>
+
+                            <div className="wf-date">
+                                <span>
+                                    {item.dt_txt}
+                                </span>
+                            </div>
+
+                            <div className="wf-temp">
+                                <label>Temp: </label>
+                                <span>
+                                    {item.main.temp} &deg;C
+                                </span>
+                            </div>
+
+                            <div className="wf-humidity">
+                                <label>Humid: </label>
+                                <span>{item.main.humidity}%</span>
+                            </div>
+
+                            <div className="wf-conditions">
+                                <img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} alt='icon'/>           
+                                <span>{item.weather[0].main}</span>
+                            </div>
+
+                        </div>
+                    )                                           
+                })}
+            </div>
+        ): (
+           <div>Not fetched correctly</div> 
+        )}
         </>
     );
 } 
